729c4db01e4f90b72cca9637c3b7491228e3f7d6
Mahima <mahima.6.ms@gmail.com>
2019-01-27 19:56:52 +0530

    fetch data asynchronously from http url
    update ui to display images and name for each pokemon

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:pokemon/pokehub.dart';
 
 void main() => runApp(MaterialApp(
-  title: "PokeApp",
-  home: HomePage(),
-  debugShowCheckedModeBanner: false,
-));
+      title: "PokeApp",
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    ));
 
 class HomePage extends StatefulWidget {
   @override
@@ -15,8 +17,10 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageState extends State<HomePage> {
+  var url =
+      "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json";
 
-  var url = "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json";
+  PokeHub pokeHub;
 
   @override
   void initState() {
@@ -27,9 +31,11 @@ class HomePageState extends State<HomePage> {
 
   void fetchData() async {
     var response = await http.get(url);
-    print(response);
+    var decodedJson = jsonDecode(response.body);
+    pokeHub = PokeHub.fromJson(decodedJson);
+    print(pokeHub.toJson());
+    setState(() {});
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -38,11 +44,45 @@ class HomePageState extends State<HomePage> {
         title: Text("PokeApp"),
         backgroundColor: Colors.cyan,
       ),
+      body: pokeHub == null
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : GridView.count(
+              crossAxisCount: 2,
+              children: pokeHub.pokemon
+                  .map((poke) => Padding(
+                        padding: const EdgeInsets.all(2.0),
+                        child: Card(
+                          elevation: 3.0,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[
+                              Container(
+                                height: 100.0,
+                                width: 100.0,
+                                decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                        image: NetworkImage(poke.img))),
+                              ),
+                              Text(
+                                poke.name,
+                                style: TextStyle(
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ))
+                  .toList(),
+            ),
       drawer: Drawer(),
-      floatingActionButton: FloatingActionButton(onPressed: () {},
-      backgroundColor: Colors.cyan,
-      child: Icon(Icons.refresh)),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: Colors.cyan,
+          child: Icon(Icons.refresh)),
     );
   }
 }
-
